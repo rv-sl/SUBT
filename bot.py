22da8f594c398ef36acce78288fba2e387faba44
rv-sl <raveemox@gmail.com>
2025-04-24 07:16:33 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+from pyrogram import Client, filters import os from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client( "auto-subtitle-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="bot/plugins") )
+
+if name == "main":
+  os.makedirs("downloads", exist_ok=True)
+  app.run()
