2fb0b111ca5cc9a0f446b4ebeddc4f06bdfd0606
rv-sl <raveemox@gmail.com>
2025-04-24 08:30:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters 
-import os from config import API_ID, API_HASH, BOT_TOKEN
+import os 
+from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client( "auto-subtitle-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="bot/plugins") )
 
