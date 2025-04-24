92cd2150bb3fe5c785611edcfe4d98ab69298b00
rv-sl <raveemox@gmail.com>
2025-04-24 08:41:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="bot/plugins")
+    plugins=dict(root="plugins")
 )
 
 if __name__ == "__main__":
