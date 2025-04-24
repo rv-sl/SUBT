3b670569b5f5e159645d1ed45f0a45b4f9f157ab
rv-sl <raveemox@gmail.com>
2025-04-24 08:34:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
-from pyrogram import Client, filters 
-import os 
+from pyrogram import Client
+import os
 from config import API_ID, API_HASH, BOT_TOKEN
 
-app = Client( "auto-subtitle-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="bot/plugins") )
+app = Client(
+    "auto-subtitle-bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="bot/plugins")
+)
 
-if name == "main":
-  os.makedirs("downloads", exist_ok=True)
-  app.run()
+if __name__ == "__main__":
+    os.makedirs("downloads", exist_ok=True)
+    app.run()
